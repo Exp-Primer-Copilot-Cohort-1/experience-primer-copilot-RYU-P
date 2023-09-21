53e6267779d6eee679d4dd5a00072f2b0fdbb308
Ryuji Yamasaki <127810261+RYU-P@users.noreply.github.com>
2023-09-21 20:37:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,16 @@
-//create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Comment = require('./models/comment');
-var Post = require('./models/post');
-var User = require('./models/user');
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-var passportLocalMongoose = require('passport-local-mongoose');
-var expressSession = require('express-session');
-var methodOverride = require('method-override');
-var flash = require('connect-flash');
-var seedDB = require('./seeds');
+// create web server for comment
+
+const exxpress = require('express')
+const router = exxpress.Router()
+const commentController = require('../controllers/commentController')
+
+//handle request
+router.get('/comments', commentController.comment_index)
+router.get('/comments/create', commentController.comment_create_get)
+router.post('/comments/create', commentController.comment_create_post)
+router.get('/comments/:id', commentController.comment_details)
+
+module.exports = router
+router.delete('/comments/:id', commentController.comment_delete)
+router.get('/comments/edit/:id', commentController.comment_edit_get)
+router.put('/comments/:id', commentController.comment_edit_put)
